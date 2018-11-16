0f839c5799b67d9181d4c25bdc5dfd576e335c7c
Pangolino91 <enrico.cerri1991@gmail.com>
2018-11-16 09:38:03 +0100

    first commit

---
+++ js/filestackprova.js
@@ -0,0 +1,13 @@
+// import * as filestack from ('filestack-js');
+
+// window.addEventListener('DOMContentLoaded', function () {
+//     const apikey = 'A9ZaZkmYdSYuVVWVsY6q6z';
+//     const client = filestack.init(apikey);
+//     const options = {
+//       maxFiles: 20,
+//       uploadInBackground: false,
+//       onOpen: () => console.log('opened!'),
+//       onUploadDone: (res) => console.log(res),
+//     };
+//     client.picker(options).open();
+//   });
